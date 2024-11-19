13340cc49c501c67db67043ccb656f67679b15d2
wznmickey <first@wznmickey.com>
2024-11-19 17:22:15 -0500

    test

--- mybash.bash
+++ mybash.bash
@@ -6,4 +6,12 @@ done
 
 mkdir dist
 
-mv slides/*/dist/* dist/
+for dir in slides/*/dist; do
+    if [ -d "$dir" ]; then 
+        source_folder=$(dirname "$dir")
+        target_name=$(echo "$source_folder" | sed 's/slides\///; s/\//_/g')
+        target_path="/dist/${target_name}_dist"
+        echo "Moving $dir to $target_path"
+        mv "$dir" "$target_path"
+    fi
+done
